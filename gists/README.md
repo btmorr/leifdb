51825af2e501a47640495e83347b51527b7bfcde
Toma Morris <btmorr@users.noreply.github.com>
2020-05-31 21:29:56 -0400

    Add gists readme and database test (#28)

---
+++ gists/README.md
@@ -0,0 +1,3 @@
+# Gists
+
+This directory contains snippets of runnable code that demonstrate a technique or concept that is either used in the application, or something that was learned while developing it.
---
+++ internal/database/db_test.go
@@ -0,0 +1,28 @@
+package database
+
+import (
+	"testing"
+)
+
+func TestDatabase(t *testing.T) {
+	d := NewDatabase()
+
+	k := "test"
+	r1 := d.Get(k)
+	if r1 != "" {
+		t.Errorf("Read empty key should return empty string, got: %s\n", r1)
+	}
+
+	v1 := "something"
+	d.Set(k, v1)
+	r2 := d.Get(k)
+	if r2 != v1 {
+		t.Errorf("Read \"%s\" should return \"%s\", got: %s\n", k, v1, r2)
+	}
+
+	d.Delete(k)
+	r3 := d.Get(k)
+	if r3 != "" {
+		t.Errorf("Read deleted key should return empty string, got: %s\n", r3)
+	}
+}
