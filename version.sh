139793ecec9ce47b5e3c27adee9b48c8380adfb0
Toma Morris <btmorr@users.noreply.github.com>
2020-06-04 10:25:59 -0400

    Make version valid semver with branch commit metadata

--- version.sh
+++ version.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
-VERSION="0.1.0b"
+SHORT_HASH=$(git rev-parse --short HEAD)
+VERSION="0.1.0-beta"
 
 if [ $BRANCH == "edge" ]
 then
     echo $VERSION
 else
-    echo $VERSION-$BRANCH
+    echo $VERSION+$BRANCH.$SHORT_HASH
 fi
