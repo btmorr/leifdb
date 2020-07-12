c7dc494ecf71608bb20d7515e4ab96ba85a5e617
Sameer Kolhar <kolhar730@gmail.com>
2020-07-12 10:21:30 +0530

    add server version and other info to health endpoint (#100)

--- docs/docs.go
+++ docs/docs.go
@@ -185,6 +185,9 @@ var doc = `{
             "properties": {
                 "status": {
                     "type": "string"
+                },
+                "version": {
+                    "type": "string"
                 }
             }
         },
