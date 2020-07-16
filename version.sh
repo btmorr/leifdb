4260453e5d1a68ea08f6550d528b4c44ca183850
Toma Morris <btmorr@users.noreply.github.com>
2020-07-15 21:38:07 -0400

    Bump verison to v0.2.0-beta.1 (#109)

--- version.sh
+++ version.sh
@@ -2,7 +2,7 @@
 
 BRANCH=$(git rev-parse --abbrev-ref HEAD | sed 's/\//-/')
 SHORT_HASH=$(git rev-parse --short HEAD)
-VERSION="0.2.0-beta.0"
+VERSION="0.2.0-beta.1"
 
 if [ $BRANCH == "edge" ]
 then
