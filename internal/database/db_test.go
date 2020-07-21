52a64e8ca827f70e1f10c6ab43baab357710cee5
Toma Morris <btmorr@users.noreply.github.com>
2020-07-20 22:50:41 -0400

    Build snapshot (#113)
    * Add methods to database for building and installing snapshot
    * Add env vars for snapshot config

--- internal/database/db_test.go
+++ internal/database/db_test.go
@@ -28,3 +28,55 @@ func TestDatabase(t *testing.T) {
 		t.Errorf("Read deleted key should return empty string, got: %s\n", r3)
 	}
 }
+
+func TestClone(t *testing.T) {
+	d0 := NewDatabase()
+	d0.Set("1", "one")
+	d0.Set("2", "two")
+	d0.Set("3", "three")
+
+	d1 := Clone(d0)
+
+	checkKey := "4"
+	checkValue := "four"
+	d0.Set(checkKey, checkValue)
+
+	baseCheck := d0.Get(checkKey)
+	if baseCheck != checkValue {
+		t.Errorf(
+			"Base instance should have been updated with %s=%s, got %s=%s\n",
+			checkKey, checkValue, checkKey, baseCheck)
+	}
+
+	branchCheck := d1.Get(checkValue)
+	if branchCheck != "" {
+		t.Errorf(
+			"Branch instance should not have been updated, got %s=%s\n",
+			checkKey, branchCheck)
+	}
+}
+
+func TestSnapshotRoundtrip(t *testing.T) {
+	d0 := NewDatabase()
+	keys := []string{"1", "2", "3"}
+	d0.Set("1", "one")
+	d0.Set("2", "two")
+	d0.Set("3", "three")
+
+	snapshot, err := BuildSnapshot(d0)
+	if err != nil {
+		t.Errorf("Error in BuildSnapshot: %v\n", err)
+	}
+	d1, err := InstallSnapshot(snapshot)
+	if err != nil {
+		t.Errorf("Error in InstallSnapshot: %v\n", err)
+	}
+
+	for _, key := range keys {
+		v0 := d0.Get(key)
+		v1 := d1.Get(key)
+		if v0 != v1 {
+			t.Errorf("Source value for key %s is %s but destination is %s\n", key, v0, v1)
+		}
+	}
+}
